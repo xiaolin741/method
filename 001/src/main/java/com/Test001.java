b914658bce0aaf0e2bd5362f05985ad2a81f2556
zs <zs@bjpowernode.com>
2019-10-19 19:28:47 +0800

    method

---
+++ 001/src/main/java/com/Test001.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * 作者：nxl
+ * 2019/10/18 0018
+ */
+public class Test001 {
+
+    public void test01(){
+
+    }
+}
---
+++ 001/src/test/java/com/Test01.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.junit.Test;
+
+/**
+ * 作者：nxl
+ * 2019/10/18 0018
+ */
+public class Test01 {
+    @Test
+    public void isFlower(){
+        int num = 200;
+        if(num >=100 && num <=999){
+            int gw = num % 10,sw = num / 10 % 10,bw = num /100;
+            int sum = gw * gw * gw + sw * sw *sw + bw * bw * bw;
+             if (sum == num){
+                 System.out.println("是");
+             }else {
+                 System.out.println("不是");
+             }
+        }else {
+            System.out.println("fou");
+        }
+    }
+}
